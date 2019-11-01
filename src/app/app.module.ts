169003f7b3e20aacb0a16eefef13e30e588a4050
Nachd-IT Startup <nachdit.service@gmail.com>
2019-11-01 13:49:35 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: createTranslateLoader,
+            deps: [HttpClient]
+        }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  constructor() { }
+  newToDo: string = '';
+  list: any[] = [];
+  ngOnInit() {
+  }
+  addToDo() {
+    let todo = {
+      id : this.list.length,
+      content: this.newToDo,
+      isDone: false,
+      isRemoved : false,
+      date : new Date()
+    }
+    this.list.push(todo)
+    this.newToDo = '';
+  }
+
+  done(i) {
+    this.list[i].isDone = true;
+  }
+  remove(i) {
+   // this.list = this.list.filter((s) => s.id != id)
+   this.list[i].isRemoved = true;
+  }
+}
